c45c67f8dd6b9e785366de42e24e827fa2060154
unknown <olayiwolaagboolaakanji@gmail.com>
2022-04-19 14:07:45 +0100

    project restart

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
 
-const api = {
+const apiKey = {
   key: "7d6a208651c140348448d18951fafb79",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+  
   return (
     <div className="App">
-      <main>
-        <div className='search-box'>
-          <input 
-          type='text'
-          className='search-bar'
-          placeholder='Search...'
-          />
-        </div>
-      </main>
+     
     </div>
   );
 }
